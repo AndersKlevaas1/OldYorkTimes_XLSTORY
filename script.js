d893bcf18e1da88cb1ade55ed215ebca984fe5f3
AndersKlevaas1 <anderkl@hiof.no>
2024-11-15 13:52:27 +0100

    oppdatert js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const people = document.querySelectorAll(".person");
+const peopleContainer = document.querySelector(".people-container");
+
+
+function checkScrollPosition() {
+    const containerTop = peopleContainer.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight * 0.5; 
+
+    if (containerTop < triggerPoint) {
+        people.forEach(person => person.classList.add("red"));
+    } else {
+        people.forEach(person => person.classList.remove("red"));
+    }
+}
+
+window.addEventListener("scroll", checkScrollPosition);
 
 
 
